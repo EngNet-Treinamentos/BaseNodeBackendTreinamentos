f2031d045b75250fadb1429923417d13445af161
arthurlleite <arthur.c.lleite@gmail.com>
2024-07-12 03:36:40 -0300

    Definição das rotas

---
+++ backend/src/routes/carro.route.ts
@@ -0,0 +1,79 @@
+import { Router } from "express";
+import { list, createCarro, findCarroByPlaca, updateCarro, deleteCarro } from "../business/carro.business";
+import createHttpError from "http-errors";
+import { CarroIdSchema, CarroSchema } from "../schemas/carro.schema";
+
+const router = Router();
+
+router.get("/", async (req, res) => {
+  const { userId } = req;
+  const carroParams = req.query;
+
+  if (userId === undefined) {
+    throw new createHttpError.Unauthorized("Usuário não autenticado");
+  }
+
+  const carros = await list(carroParams);
+
+  return res.status(200).json(carros);
+});
+
+router.get("/:id", async (req, res) => {
+  const { userId } = req;
+
+  if (userId === undefined) {
+    throw new createHttpError.Unauthorized("Usuário não autenticado");
+  }
+
+  const placa = CarroIdSchema.parse(req.params.id);
+  const carro = await findCarroByPlaca(placa);
+
+  if (carro === null) {
+    throw new createHttpError.NotFound("Carro not found");
+  }
+
+  return res.status(200).json(carro);
+});
+
+router.post("/", async (req, res) => {
+  const { userId } = req;
+
+  if (userId === undefined) {
+    throw new createHttpError.Unauthorized("Usuário não autenticado");
+  }
+
+  const carroData = CarroSchema.parse(req.body);
+  const carro = await createCarro(carroData);
+
+  return res.status(201).json(carro);
+});
+
+router.put("/:id", async (req, res) => {
+  const { userId } = req;
+
+  if (userId === undefined) {
+    throw new createHttpError.Unauthorized("Usuário não autenticado");
+  }
+
+  const placa = CarroIdSchema.parse(req.params.id);
+  const carroData = CarroSchema.parse(req.body);
+
+  await updateCarro({ ...carroData, placa });
+
+  return res.status(204).send();
+});
+
+router.delete("/:id", async (req, res) => {
+  const { userId } = req;
+
+  if (userId === undefined) {
+    throw new createHttpError.Unauthorized("Usuário não autenticado");
+  }
+
+  const id = CarroIdSchema.parse(req.params.id);
+  await deleteCarro(id);
+
+  return res.status(204).send();
+});
+
+export default router;
---
+++ backend/src/routes/motorista.route.ts
@@ -0,0 +1,114 @@
+import { Router } from "express";
+import {
+  list,
+  createMotorista,
+  findMotoristaByCpf,
+  updateMotorista,
+  deleteMotorista,
+} from "../business/motorista.business";
+import { findCarroByMotoristaId } from "../business/carro.business";
+import { findMultasByMotoristaCpf } from "../business/multa.business";
+import { MotoristaIdSchema, MotoristaSchema } from "../schemas/motorista.schema";
+import createHttpError from "http-errors";
+
+const router = Router();
+
+router.get("/", async (req, res) => {
+  const { userId } = req;
+
+  if (userId === undefined) {
+    throw new createHttpError.Unauthorized("Usuário não autenticado");
+  }
+
+  const motoristaParams = req.query;
+
+  const motoristas = await list(motoristaParams);
+
+  return res.status(200).json(motoristas);
+});
+
+router.get("/:id", async (req, res) => {
+  const { userId } = req;
+
+  if (userId === undefined) {
+    throw new createHttpError.Unauthorized("Usuário não autenticado");
+  }
+
+  const cpf = MotoristaIdSchema.parse(req.params.id);
+  const motorista = await findMotoristaByCpf(cpf);
+
+  if (motorista === null) {
+    throw new createHttpError.NotFound("Motorista not found");
+  }
+
+  return res.status(200).json(motorista);
+});
+
+router.get("/:id/carros", async (req, res) => {
+  const { userId } = req;
+
+  if (userId === undefined) {
+    throw new createHttpError.Unauthorized("Usuário não autenticado");
+  }
+
+  const id = MotoristaIdSchema.parse(req.params.id);
+  const carros = await findCarroByMotoristaId(id);
+
+  return res.status(200).json(carros);
+});
+
+router.get("/:id/multas", async (req, res) => {
+  const { userId } = req;
+
+  if (userId === undefined) {
+    throw new createHttpError.Unauthorized("Usuário não autenticado");
+  }
+
+  const cpf = MotoristaIdSchema.parse(req.params.id);
+  const multas = await findMultasByMotoristaCpf(cpf);
+
+  return res.status(200).json(multas);
+});
+
+router.post("/", async (req, res) => {
+  const { userId } = req;
+
+  if (userId === undefined) {
+    throw new createHttpError.Unauthorized("Usuário não autenticado");
+  }
+
+  const motoristaData = MotoristaSchema.parse(req.body);
+  const motorista = await createMotorista(motoristaData);
+
+  return res.status(201).json(motorista);
+});
+
+router.put("/:id", async (req, res) => {
+  const { userId } = req;
+
+  if (userId === undefined) {
+    throw new createHttpError.Unauthorized("Usuário não autenticado");
+  }
+
+  const cpf = MotoristaIdSchema.parse(req.params.id);
+  const motoristaData = MotoristaSchema.parse(req.body);
+
+  const motorista = await updateMotorista({ ...motoristaData, cpf });
+
+  return res.status(200).json(motorista);
+});
+
+router.delete("/:id", async (req, res) => {
+  const { userId } = req;
+
+  if (userId === undefined) {
+    throw new createHttpError.Unauthorized("Usuário não autenticado");
+  }
+
+  const id = MotoristaIdSchema.parse(req.params.id);
+  await deleteMotorista(id);
+
+  return res.status(204).send();
+});
+
+export default router;
